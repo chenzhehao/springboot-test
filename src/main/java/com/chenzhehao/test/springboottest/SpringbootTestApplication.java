488556d404859d728ae6f827e16d5a986b502482
zhehao.chen <852347470@qq.com>
2024-11-19 10:23:51 +0800

    first commit

---
+++ src/main/java/com/chenzhehao/test/springboottest/SpringbootTestApplication.java
@@ -0,0 +1,20 @@
+package com.chenzhehao.test.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public Object jvmboot(){
+        return "success jvm";
+    }
+}
